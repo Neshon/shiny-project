66fef77237b6f827bfa69b30d524d0722a6a37a8
hifer11 <hifer11@yandex.ua>
2022-04-15 12:19:52 +0300

    add app chat and channels

--- shiny_project/settings.py
+++ shiny_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chat',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -121,5 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
